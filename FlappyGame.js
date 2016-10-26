45481030c665003d62a0074be7535dc6f9f9cc9e
Sofus Heggemsens <sofeous@gmail.com>
2016-10-26 14:54:44 +0200

    added the game file
    started the game and made a game area

---
+++ FlappyGame.js
@@ -0,0 +1,13 @@
+﻿function startGame() {
+    gameArea.start();
+}
+
+gameArea = {
+    canvas : document.createElement('canvas');
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
