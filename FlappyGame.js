80e314e21ad0a98a67caebaeaeba8e9f9166fc4a
Sofus Heggemsens <sofeous@gmail.com>
2016-10-31 22:48:18 +0100

    putt in stuff
    added a constructor for the game pieces
    made the game area update
    made a bird(the brown square);

--- FlappyGame.js
+++ FlappyGame.js
@@ -1,13 +1,38 @@
-﻿function startGame() {
+var bird;
+
+function startGame() {
     gameArea.start();
+    bird = new Component(30, 30, "brown", 10, 120);
 }
 
 gameArea = {
-    canvas : document.createElement('canvas');
+    canvas : document.createElement('canvas'),
     start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
-        document.insertBefore(this.canvas, document.body.childNodes[0]);
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
+}
+
+function Component (width, height, color, x, y){
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = gameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function updateGameArea() {
+    gameArea.clear;
+    bird.update();
+    
 }
